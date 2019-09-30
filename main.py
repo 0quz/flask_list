4e2b7392e6fcbef0ea33cc80fd5833b7701eb6d8
Oğuz A. Çakır <oguz.28414@gmail.com>
2019-09-30 09:20:53 +0300

    list.html'den arama bölümü çıkartılıp, anasayfaya eklendi. Bazı durumlar düzeltildi.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('oguzabdullahcakir.html')
+    return render_template('home.html')
 
 
 @app.route('/list', methods=['POST', 'GET'])
@@ -15,8 +15,9 @@ def list():
     if request.args.get('s') and request.args.get('f'):
         startdate = request.args.get('s')
         finishdate = request.args.get('f')
-        url = "http://staging1.alo-tech.com/api/?function=reportsCDRLogs&startdate={}%2012:00:00&finishdate={}%2013:00:00&app_token=ag9zfnRlbGVmb25pLXRlc3RyHwsSElRlbmFudEFwcGxpY2F0aW9ucxiAgICw46OcCQyiARVzdGFnaW5nMS5hbG8tdGVjaC5jb20"\
-            .format(startdate, finishdate)
+        App_token = "app_token=ag9zfnRlbGVmb25pLXRlc3RyHwsSElRlbmFudEFwcGxpY2F0aW9ucxiAgICw46OcCQyiARVzdGFnaW5nMS5hbG8tdGVjaC5jb20"
+        url = "http://staging1.alo-tech.com/api/?function=reportsCDRLogs&startdate={}%2012:00:00&finishdate={}%2013:00:00&{}"\
+            .format(startdate, finishdate, App_token)
         r = requests.get(url)
         data = r.json()
 
